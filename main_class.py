b74ff385c013033cf14221eac88a1f17c74d178d
BalabanovNikita <balabanov98.16@gmail.com>
2022-03-04 17:31:57 +0300

    Fixed:)

--- main_class.py
+++ main_class.py
@@ -9,11 +9,11 @@ json_file = "/media/nikita/HDD/datasets/METU-ALET/ALET/trainv4.json"
 
 img_folder = "/media/nikita/HDD/datasets/xView/train_images/train_images"
 txt_folder = "/media/nikita/HDD/datasets/xView/train_labels/xView_train.geojson"
-out_img_folder = "/media/nikita/HDD/test/img/"
-out_txt_folder = "/media/nikita/HDD/test/txt/"
-Img_mosaic = MosaicController(images, json_file, out_img_folder, out_txt_folder, METU)
+out_img_folder = "/media/nikita/HDD/test/xView/img/"
+out_txt_folder = "/media/nikita/HDD/test/xView/txt/"
+Img_mosaic = MosaicController(img_folder, txt_folder, out_img_folder, out_txt_folder, XVIEW)
 Img_mosaic.set_start_count(0)
-Img_mosaic.set_end_count(50)
+Img_mosaic.set_end_count(2000)
 Img_mosaic.make_mosaic()
 end = time.time()
 print(end - start)
--- utils/convertors.py
+++ utils/convertors.py
@@ -12,6 +12,19 @@ def from_yolo_to_rec(x: int, y: int, width: int, height: int, image_width: int, 
         y_max = int(y_mid + box_h / 2) - 1
         return [x_min, y_min, x_max, y_max]
 
+def from_yolo_to_rec(box: list, image_width: int, image_height: int) -> list:
+        """
+        This method turns annotations in YOLO format into rectangle coordinates
+        """
+        box_w = int(box[2] * image_width)
+        box_h = int(box[3] * image_height)
+        x_mid = int(box[0] * image_width + 1)
+        y_mid = int(box[1] * image_height + 1)
+        x_min = int(x_mid - box_w / 2) + 1
+        x_max = int(x_mid + box_w / 2) - 1
+        y_min = int(y_mid - box_h / 2) + 1
+        y_max = int(y_mid + box_h / 2) - 1
+        return [x_min, y_min, x_max, y_max]
 
 def from_rec_to_yolo(x1: int, y1: int, x2: int, y2: int, image_width: int, image_height: int) -> list:
         """
